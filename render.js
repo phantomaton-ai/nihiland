cb79f9823e568762e1e2879e45c073d2a8085166
Doc Woe <woe@phantomaton.com>
2025-04-16 23:55:52 +0000

    render

--- render.js
+++ render.js
@@ -1,13 +1,44 @@
 import fs from 'fs';
-import glob from 'glob';
-import marked from 'marked';
+import * as glob from 'glob';
+import { marked } from 'marked';
+import metamagic from 'metamagic';
 import necronomicon from 'necronomicon';
 import path from 'path';
+import phantasia from 'phantasia';
 
 const BUILT = 'built';
 const PARK = 'park';
 
-const commands = []; // TODO: 'image'
+const TEMPLATE = body => `<html>
+<head>
+<title>Nihiland</title>
+</head>
+<body>
+${body}
+</body>
+</html>`
+
+
+const commands = [metamagic(
+  'image',
+  async ({ file }, prompt) => {
+    const output = path.join(BUILT, file);
+    if (!fs.existsSync(output)) {
+      await phantasia(prompt, { output });
+    }
+    return `![${prompt}](${file})\n`;
+  },
+  {
+    attributes: {
+      file: {
+        description: 'The name of the PNG file to write.'
+      }
+    },
+    body: {
+      description: 'A prompt describing the image, to be used for image generation.'
+    }
+  }
+)];
 
 const spellbook = necronomicon({ commands, includes: {
   directives: false,
@@ -16,3 +47,12 @@ const spellbook = necronomicon({ commands, includes: {
 } });
 
 const sources = glob.globSync(path.join(PARK, '**', '*.md'));
+
+for (const source of sources) {
+  const text = fs.readFileSync(source, 'utf-8');
+  const executed = await spellbook.execute(text);
+  const parsed = marked.parse(executed);
+  const templated = TEMPLATE(parsed);
+  const destination = source.replace(PARK, BUILT).replace(/\.md$/, '.html');
+  fs.writeFileSync(destination, templated, 'utf-8');
+}
